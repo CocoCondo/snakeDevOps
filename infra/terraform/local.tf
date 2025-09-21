f4a62176f3457d340002a4e6b5a672bb28ffc22b
Ricardo <cardo88@gmail.com>
2025-09-20 22:35:44 -0300

    RC - 2025-09-20 - terraform init

---
+++ infra/terraform/local.tf
@@ -0,0 +1,9 @@
+locals {
+  labels = {
+    project = "snake-devops"
+    managed = "terraform"
+  }
+
+  # Archivo estático para Nginx (se monta como bind)
+  nginx_index_html = "${path.module}/index.html"
+}
